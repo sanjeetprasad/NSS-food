b7cdf636628ded3a7f6a0de7b92719cb7ae907fd
Sanjeet Prasad <sanjeet.l.prasad@gmail.com>
2020-04-28 13:46:49 -0500

    lesson soleved

---
+++ main.js
@@ -0,0 +1,42 @@
+// string //
+/*let food = "lunch";
+let breakfast = "peanut butter and jelly";
+let bread = "whole wheat";
+let juice = "grape juice";
+let fruit = "apple";
+
+console.log(food, brackfast, bread, juice, fruit)
+*/
+
+let food = "lunch, peanut butter and jelly, whole wheat, grape juice, apple";
+
+console.log(food)
+
+
+
+
+
+// object //
+ let food_2 = {
+    type: "lunch",
+    brackfast: "peanut butter and jelly",
+    bread: "whole wheat",
+    juice: "grape juice"
+
+};
+
+console.log(food_2)
+
+
+
+
+// arrays //
+ let food_3 = ["lunch", "peanut butter and jelly", "whole wheat", "grape juice"]; 
+
+ console.log(food_3)
+
+
+
+
+
+
